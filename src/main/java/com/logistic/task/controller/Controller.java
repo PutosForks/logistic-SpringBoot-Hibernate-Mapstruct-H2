d1d8787cfbe5230b431121e2e3034000d7be1338
slilex <mrslilex@gmail.com>
2019-05-16 05:02:26 +0300

    commit

--- src/main/java/com/logistic/task/controller/Controller.java
+++ src/main/java/com/logistic/task/controller/Controller.java
@@ -22,6 +22,24 @@ public class Controller {
                 +"get /clients/5 - вывод всех клиета с ID 5 "+ "\n"
                 +"post /clients/ - сохранение переданного Json в объект и в базу" +"\n"
                 +"put /clients/ID - обновление переданого в Json обекта по ID" +"\n"
-                +"delete /clients/ID - удаление клиента по ID";
+                +"delete /clients/ID - удаление клиента по ID"+
+                "\n"+
+                "all entitys - Client\n" +
+                "     Address\n" +
+                "   Crew\n" +
+                "    Flight\n" +
+                "    Person\n" +
+                "    Shift\n" +
+                "    Vehicle\n" +
+                "\n"+"\n"+"\n"+"\n"+
+                "all Request"+
+                "\n"+"\n"+"\n"+"\n"+
+                "/clients"+"\n"+
+                "/address"+"\n"+
+                "/crew"+"\n"+
+                "/flight"+"\n"+
+                "/person"+"\n"+
+                "/shift"+"\n"+
+                "/vehicle"+"\n";
     }
 }
